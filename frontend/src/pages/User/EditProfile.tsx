90f904fa4c17dfa9ac4df028e96e42b750509b66
rohith-s-s <rohithss2001@gmail.com>
2024-10-07 11:08:52 +0530

    Remove unused imports and variables in EditProfile component.

--- frontend/src/pages/User/EditProfile.tsx
+++ frontend/src/pages/User/EditProfile.tsx
@@ -1,13 +1,8 @@
-import { useQueryClient } from "@tanstack/react-query"
 import AuthForm from "../../components/AuthForm"
-import { userData } from "../../types/util"
 import { useUpdate } from "../../hooks/useUser"
 
 const EditProfile = () => {
-    const queryClinet = useQueryClient()
-    const data:userData = queryClinet.getQueryData(['user']) as userData
     const {mutate,isError,error} = useUpdate()
-    console.log(data._id)
     return (
             <>
                 <AuthForm logic="Edit" error={error} isError={isError} mutate={mutate}/>
